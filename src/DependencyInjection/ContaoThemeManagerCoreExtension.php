49ae4c043cbcdbb26592fe87dd475a0f09c230b1
eki89 <fabian@oveleon.de>
2022-12-06 17:41:01 +0100

    Additional default variables added to news templates

---
+++ src/DependencyInjection/ContaoThemeManagerCoreExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ContaoThemeManager\Core\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoThemeManagerCoreExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
---
+++ src/EventListener/ParseArticlesListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ContaoThemeManager\Core\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Date;
+use Contao\FrontendTemplate;
+use Contao\Module;
+use Contao\PageModel;
+
+#[AsHook('parseArticles')]
+class ParseArticlesListener
+{
+    public function __invoke(FrontendTemplate $template, array $newsEntry, Module $module): void
+    {
+        /** @var PageModel $objPage */
+        global $objPage;
+
+        // Add various date and time variables
+        $template->date = Date::parse($objPage->dateFormat, $newsEntry['tstamp']);
+        $template->time = Date::parse($objPage->timeFormat, $newsEntry['tstamp']);
+        $template->dayMonth = Date::parse('d. F', $newsEntry['tstamp']);
+        $template->dayMonthShort = Date::parse('d. M.', $newsEntry['tstamp']);
+        $template->year = Date::parse('Y', $newsEntry['tstamp']);
+
+        // Add author name without additions
+        $template->authorName = str_replace($GLOBALS['TL_LANG']['MSC']['by'] . ' ', '', $template->author);
+
+        // Modify comment variables
+        if ($template->numberOfComments === 1)
+        {
+            $template->commentCount = $GLOBALS['TL_LANG']['MSC']['commentCountOne'];
+        }
+        else
+        {
+            $template->commentCount = sprintf($GLOBALS['TL_LANG']['MSC']['commentCountMultiple'], $template->numberOfComments);
+        }
+
+        $template->hasComments = !!$template->numberOfComments;
+    }
+}
