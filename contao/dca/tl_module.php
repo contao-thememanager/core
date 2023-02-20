249baaada083d6bf3a271fc19db9c63b4ad79e9d
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-02-20 19:13:31 +0100

    [Update] Only add needed module fields when bundles are installed
    - FAQ
    - News

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -37,41 +37,33 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['headline2Style'] = [
     'sql'         => "varchar(2) NOT NULL default ''"
 ];
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['showFaqInfo'] = [
-    'exclude'     => true,
-    'inputType'   => 'checkbox',
-    'eval'        => ['tl_class'=>'w50 m12'],
-    'sql'         => "char(1) NOT NULL default ''"
-];
+$GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = ['ContaoThemeManager\Core\ThemeManager', 'extendHeadlineField'];
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['faqAccordion'] = [
-    'exclude'     => true,
-    'inputType'   => 'select',
-    'options'     => ['single_open', 'single_closed', 'multi_open', 'multi_closed'],
-    'reference'   => &$GLOBALS['TL_LANG']['tl_module']['faqAccordion'],
-    'eval'        => ['tl_class'=>'w50'],
-    'sql'         => "varchar(16) COLLATE ascii_bin NOT NULL default 'single_open'"
-];
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['news_removeBy'] = [
-    'exclude'     => true,
-    'inputType'   => 'checkbox',
-    'eval'        => ['tl_class'=>'w100'],
-    'sql'         => "char(1) NOT NULL default ''"
-];
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['news_datimFormat'] = [
-    'exclude'     => true,
-    'inputType'   => 'text',
-    'eval'        => ['helpwizard'=>true, 'decodeEntities'=>true, 'placeholder'=>Config::get('datimFormat'), 'tl_class'=>'w50'],
-    'explanation' => 'dateFormat',
-    'sql'         => "varchar(32) NOT NULL default ''"
-];
-
+/**
+ * Get bundle information
+ */
 $bundles = System::getContainer()->getParameter('kernel.bundles');
 
+/**
+ * Add News bundle related fields
+ */
 if (isset($bundles['ContaoNewsBundle']))
 {
+    $GLOBALS['TL_DCA']['tl_module']['fields']['news_removeBy'] = [
+        'exclude'     => true,
+        'inputType'   => 'checkbox',
+        'eval'        => ['tl_class'=>'w100'],
+        'sql'         => "char(1) NOT NULL default ''"
+    ];
+
+    $GLOBALS['TL_DCA']['tl_module']['fields']['news_datimFormat'] = [
+        'exclude'     => true,
+        'inputType'   => 'text',
+        'eval'        => ['helpwizard'=>true, 'decodeEntities'=>true, 'placeholder'=>Config::get('datimFormat'), 'tl_class'=>'w50'],
+        'explanation' => 'dateFormat',
+        'sql'         => "varchar(32) NOT NULL default ''"
+    ];
+
     PaletteManipulator::create()
         ->addField('news_removeBy', 'news_metaFields')
         ->applyToPalette('newslist', 'tl_module')
@@ -86,8 +78,27 @@ if (isset($bundles['ContaoNewsBundle']))
     ;
 }
 
+/**
+ * Add FAQ bundle related fields
+ */
 if (isset($bundles['ContaoFaqBundle']))
 {
+    $GLOBALS['TL_DCA']['tl_module']['fields']['showFaqInfo'] = [
+        'exclude'     => true,
+        'inputType'   => 'checkbox',
+        'eval'        => ['tl_class'=>'w50 m12'],
+        'sql'         => "char(1) NOT NULL default ''"
+    ];
+
+    $GLOBALS['TL_DCA']['tl_module']['fields']['faqAccordion'] = [
+        'exclude'     => true,
+        'inputType'   => 'select',
+        'options'     => ['single_open', 'single_closed', 'multi_open', 'multi_closed'],
+        'reference'   => &$GLOBALS['TL_LANG']['tl_module']['faqAccordion'],
+        'eval'        => ['tl_class'=>'w50'],
+        'sql'         => "varchar(16) COLLATE ascii_bin NOT NULL default 'single_open'"
+    ];
+
     PaletteManipulator::create()
         ->addField('showFaqInfo', 'faq_categories')
         ->applyToPalette('faqpage', 'tl_module')
@@ -95,5 +106,3 @@ if (isset($bundles['ContaoFaqBundle']))
 
     $GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = ['ContaoThemeManager\Core\ThemeManager', 'extendFaqAccordionSettings'];
 }
-
-$GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = ['ContaoThemeManager\Core\ThemeManager', 'extendHeadlineField'];
