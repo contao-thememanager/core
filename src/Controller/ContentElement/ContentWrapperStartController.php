3f16456c7bae5ee73ba7d0f0f98e27dfbe54f69f
Sebastian Zoglowek <sebastian@oveleon.de>
2023-04-05 17:56:54 +0200

    [Update] Correctly load backend templates for wrapper elements

--- src/Controller/ContentElement/ContentWrapperStartController.php
+++ src/Controller/ContentElement/ContentWrapperStartController.php
@@ -10,9 +10,11 @@ declare(strict_types=1);
 
 namespace ContaoThemeManager\Core\Controller\ContentElement;
 
+use Contao\BackendTemplate;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\System;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +26,12 @@ class ContentWrapperStartController extends AbstractContentElementController
 
     protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
     {
+        // Do not display template in backend
+        if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+        {
+            $template = new BackendTemplate('be_wildcard');
+        }
+
         return $template->getResponse();
     }
 }
