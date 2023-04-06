b9d15ad57f4884dce6272530376fb8f21b26ab56
Sebastian Zoglowek <sebastian@oveleon.de>
2023-04-06 10:12:46 +0200

    [Update] Only fetch tab group items once

--- src/Controller/ContentElement/TabStartController.php
+++ src/Controller/ContentElement/TabStartController.php
@@ -25,6 +25,8 @@ class TabStartController extends AbstractContentElementController
 {
     public const TYPE = 'tabStart';
 
+    private static array $tabStartElements = [];
+
     protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
     {
         $container = System::getContainer();
@@ -37,32 +39,56 @@ class TabStartController extends AbstractContentElementController
         }
         else
         {
-            $cols = ["tl_content.pid=? AND tl_content.type=? AND tl_content.tabGroup=?"];
+            $labels = [];
 
-            if (!$container->get('contao.security.token_checker')->isPreviewMode())
-            {
-                $time   = Date::floorToMinute();
-                $cols[] = "tl_content.invisible='' 
-                           AND (tl_content.start='' OR tl_content.start<='$time') 
-                           AND (tl_content.stop='' OR tl_content.stop>'$time')";
-            }
-
-            $template->tabGroup = $model->tabGroup;
+            $template->tabGroup = $group = !!$model->tabGroup ? $model->tabGroup : 'tab_global';
             $template->tabLabel = $model->tabLabel;
 
-            $tabElements = ContentModel::findBy(
-                $cols,
-                [$model->pid, 'tabStart', $model->tabGroup],
-                ['order' => 'sorting ASC']
-            );
-
-            if ($model->id === $tabElements->first()->id)
+            // Only build navigation for first item in every tab group
+            if (null === self::getTabStartElements($model->pid, $group))
             {
-                $template->tabNav = true;
-                $template->tabNavElements = $tabElements->fetchEach('tabLabel') ?? null;
+                $cols = ["tl_content.pid=? AND tl_content.type=? AND tl_content.tabGroup=?"];
+
+                if (!$container->get('contao.security.token_checker')->isPreviewMode())
+                {
+                    $time   = Date::floorToMinute();
+                    $cols[] = "tl_content.invisible='' 
+                           AND (tl_content.start='' OR tl_content.start<='$time') 
+                           AND (tl_content.stop='' OR tl_content.stop>'$time')";
+                }
+
+                $tabElements = ContentModel::findBy(
+                    $cols,
+                    [$model->pid, 'tabStart', $model->tabGroup],
+                    ['order' => 'sorting ASC']
+                );
+
+                if (
+                    null !== $tabElements &&
+                    !empty($labels[$model->pid][$group] = $tabElements->fetchEach('tabLabel'))
+                )
+                {
+                    self::$tabStartElements = $labels;
+
+                    $template->tabNav = true;
+                    $template->tabNavElements = $labels[$model->pid][$group];
+                }
             }
         }
 
         return $template->getResponse();
+    }
+
+    /**
+     * Gets the previously set tabs
+     */
+    private function getTabStartElements($pid, $group): ?array
+    {
+        if (isset(self::$tabStartElements[$pid][$group]))
+        {
+            return self::$tabStartElements[$pid][$group];
+        }
+
+        return null;
     }
 }
