a127362aeeaf6752e36931e1910f1582fbdd3f09
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-06-09 19:30:57 +0200

    Use legacy content-element templates

--- contao/config/config.php
+++ contao/config/config.php
@@ -43,3 +43,22 @@ $GLOBALS['TL_WRAPPERS']['stop'][]  = 'wrapperStopContent';
 
 // Hooks
 $GLOBALS['TL_HOOKS']['parseTemplate'][]    = ['ContaoThemeManager\Core\ThemeManager', 'addHeadlineFieldsToTemplate'];
+
+// Restore legacy elements -> Contao 5.1 compatibility since Twig is not supported by StyleManager yet
+// Will be removed with ThemeManager 2.1 / 2.2
+$GLOBALS['TL_CTE']['texts']['code']      = \Contao\ContentCode::class;
+$GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
+$GLOBALS['TL_CTE']['texts']['html']      = \Contao\ContentHtml::class;
+$GLOBALS['TL_CTE']['texts']['list']      = \Contao\ContentList::class;
+$GLOBALS['TL_CTE']['texts']['text']      = \Contao\ContentText::class;
+$GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
+$GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
+$GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
+$GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
+$GLOBALS['TL_CTE']['media']['gallery']   = \Contao\ContentGallery::class;
+$GLOBALS['TL_CTE']['media']['player']    = \Contao\ContentPlayer::class;
+$GLOBALS['TL_CTE']['media']['youtube']   = \Contao\ContentYouTube::class;
+$GLOBALS['TL_CTE']['media']['vimeo']     = \Contao\ContentVimeo::class;
+$GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
+$GLOBALS['TL_CTE']['files']['download']  = \Contao\ContentDownload::class;
+$GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
