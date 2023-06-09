913eb04259039a965136156e080b73d2a73751c8
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-06-09 20:13:13 +0200

    Only load legacy templates in contao 5.x

--- contao/config/config.php
+++ contao/config/config.php
@@ -7,6 +7,8 @@
  */
 
 // Contao ThemeManager
+use Contao\CoreBundle\ContaoCoreBundle;
+
 $GLOBALS['CTM_SETTINGS']['iconFont'] = '';
 
 // Register the supported CSS units for ThemeManager
@@ -45,22 +47,23 @@ $GLOBALS['TL_WRAPPERS']['stop'][]  = 'wrapperStopContent';
 $GLOBALS['TL_HOOKS']['parseTemplate'][]    = ['ContaoThemeManager\Core\ThemeManager', 'addHeadlineFieldsToTemplate'];
 
 // Restore legacy elements -> Contao 5.1 compatibility since Twig is not supported by StyleManager yet
-// Will be removed with ThemeManager 2.1 / 2.2
-$GLOBALS['TL_CTE']['texts']['code']      = \Contao\ContentCode::class;
-$GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
-$GLOBALS['TL_CTE']['texts']['html']      = \Contao\ContentHtml::class;
-$GLOBALS['TL_CTE']['texts']['list']      = \Contao\ContentList::class;
-$GLOBALS['TL_CTE']['texts']['text']      = \Contao\ContentText::class;
-$GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
-$GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
-$GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
-$GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
-$GLOBALS['TL_CTE']['media']['gallery']   = \Contao\ContentGallery::class;
-$GLOBALS['TL_CTE']['media']['youtube']   = \Contao\ContentYouTube::class;
-$GLOBALS['TL_CTE']['media']['vimeo']     = \Contao\ContentVimeo::class;
-$GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
-$GLOBALS['TL_CTE']['files']['download']  = \Contao\ContentDownload::class;
-$GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
-
-
-$GLOBALS['TL_CTE']['media']['player']    = \Contao\ContentPlayer::class;
+// Will be removed with ThemeManager 2.1
+if (str_starts_with(ContaoCoreBundle::getVersion(), '5.'))
+{
+    $GLOBALS['TL_CTE']['texts']['code']      = \Contao\ContentCode::class;
+    $GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
+    $GLOBALS['TL_CTE']['texts']['html']      = \Contao\ContentHtml::class;
+    $GLOBALS['TL_CTE']['texts']['list']      = \Contao\ContentList::class;
+    $GLOBALS['TL_CTE']['texts']['text']      = \Contao\ContentText::class;
+    $GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
+    $GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
+    $GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
+    $GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
+    $GLOBALS['TL_CTE']['media']['gallery']   = \Contao\ContentGallery::class;
+    $GLOBALS['TL_CTE']['media']['player']    = \Contao\ContentPlayer::class;
+    $GLOBALS['TL_CTE']['media']['youtube']   = \Contao\ContentYouTube::class;
+    $GLOBALS['TL_CTE']['media']['vimeo']     = \Contao\ContentVimeo::class;
+    $GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
+    $GLOBALS['TL_CTE']['files']['download']  = \Contao\ContentDownload::class;
+    $GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
+}
