653b5636a46a2bf19c173cad91bdeb58d17a226e
Sebastian Zoglowek <zumklo@googlemail.com>
2023-06-22 14:58:00 +0200

    Add default image sizes

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,11 +14,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use ContaoThemeManager\Core\ContaoThemeManagerCore;
+use Exception;
 use Oveleon\ContaoComponentStyleManager\ContaoComponentStyleManager;
 use Oveleon\ContaoThemeCompilerBundle\ContaoThemeCompilerBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface
 {
     /**
      * {@inheritdoc}
@@ -30,5 +33,13 @@ class Plugin implements BundlePluginInterface
                 ->setLoadAfter([ContaoCoreBundle::class, ContaoComponentStyleManager::class, ContaoThemeCompilerBundle::class])
                 ->setReplace(['contao-thememanager']),
         ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
+    {
+        $loader->load('@ContaoThemeManagerCore/config/config.yaml');
     }
 }
--- src/DependencyInjection/ContaoThemeManagerCoreExtension.php
+++ src/DependencyInjection/ContaoThemeManagerCoreExtension.php
@@ -2,6 +2,7 @@
 
 namespace ContaoThemeManager\Core\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -11,8 +12,9 @@ class ContaoThemeManagerCoreExtension extends Extension
 {
     /**
      * {@inheritDoc}
+     * @throws Exception
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
