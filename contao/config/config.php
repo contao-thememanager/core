debb8579f9df7a2c10a8c5a4b21af609471a10fc
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-03-18 16:36:40 +0100

    Twig integration
    - headline

--- contao/config/config.php
+++ contao/config/config.php
@@ -47,18 +47,18 @@ $GLOBALS['TL_WRAPPERS']['stop'][]  = 'wrapperStopContent';
 // Will be removed with ThemeManager 2.1
 if (str_starts_with(ContaoCoreBundle::getVersion(), '5.'))
 {
-    $GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
+    //$GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
     $GLOBALS['TL_CTE']['texts']['list']      = \Contao\ContentList::class;
     //$GLOBALS['TL_CTE']['texts']['text']      = \Contao\ContentText::class;
-    $GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
-    $GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
-    $GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
-    $GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
+    //$GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
+    //$GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
+    //$GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
+    //$GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
     $GLOBALS['TL_CTE']['media']['gallery']   = \Contao\ContentGallery::class;
     $GLOBALS['TL_CTE']['media']['player']    = \Contao\ContentPlayer::class;
     $GLOBALS['TL_CTE']['media']['youtube']   = \Contao\ContentYouTube::class;
     $GLOBALS['TL_CTE']['media']['vimeo']     = \Contao\ContentVimeo::class;
-    $GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
+    //$GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
     $GLOBALS['TL_CTE']['files']['download']  = \Contao\ContentDownload::class;
-    $GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
+    //$GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
 }
