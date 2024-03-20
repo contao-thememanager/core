463b1594ff3e82fe8a062dae85eb09c8d53d687e
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-03-20 14:58:39 +0100

    Add dynamic background image sizes

--- src/Generator/ConfigGenerator.php
+++ src/Generator/ConfigGenerator.php
@@ -8,6 +8,7 @@
 
 namespace ContaoThemeManager\Core\Generator;
 
+use ContaoThemeManager\Core\StyleManager\StyleManagerXML;
 use ContaoThemeManager\Core\ThemeManager;
 
 /**
@@ -17,69 +18,25 @@ use ContaoThemeManager\Core\ThemeManager;
  */
 class ConfigGenerator
 {
-    /**
-     * Gets all image text widths for the image-text components adds it to the style-manager-tm-config.xml
-     */
-    public function generateImageTextWidths($configVars, $xml): void
-    {
-        $options = self::getListOptions($configVars, 'image-text-ratio-options', 'it-width-', '%', [25,33,38,40,50,80,100]);
-
-        $xml->addGroup('cLayout')->addChild('imageTextWidth', $options);
-    }
+    public array $configVars;
+    public StyleManagerXML $xml;
 
     /**
-     * Gets all vertical article heights from the ThemeManager configuration and adds it to the style-manager-tm-config.xml
+     * Generates the config vars
+     *
+     * @throws \Exception
+     * @internal
      */
-    public function generateArticleHeight($configVars, $xml): void
+    public function generate(array $configVars, StyleManagerXML $xml): void
     {
-        $options    = self::getListOptions($configVars, 'article-options-vheight', 'a-vh-', 'vh', [50,75,100]);
-        $additional = self::getListOptions($configVars, 'article-options-height', 'a-h-');
+        $this->configVars = $configVars;
+        $this->xml = $xml;
 
-        $xml->addGroup('cArticleHeight')->addChild('height', array_merge($options, $additional));
-    }
-
-    /**
-     * Gets all aspect ratios from the ThemeManager configuration and adds it to the style-manager-tm-config.xml
-     */
-    public function generateAspectRatios($configVars, $xml): void
-    {
-        $options = [];
-
-        // Fallback to default values
-        if (null === ($aspectRatios = self::getThemeManagerConfigVar($configVars, 'aspect-ratios')))
-        {
-            $aspectRatios = ['(4:5)','(1:1)','(4:3)','(3:2)','(16:10)','(16:9)'];
-        }
-        else
-        {
-            $aspectRatios = explode(',', $aspectRatios);
-        }
-
-        foreach ($aspectRatios as $value)
-        {
-            if (!!$value = substr($value, 1, -1))
-            {
-                if (2 === count($ratios = explode(':', $value)))
-                {
-                    $options[] = ['key' =>'ar-'.$ratios[0].'-'.$ratios[1],'value' => $value];
-                }
-            }
-        }
-
-        $xml->addGroup('eImage')->addChild('aspectRatio', $options);
-    }
-
-    /**
-     * Gets a specific value from the ThemeManager configuration
-     */
-    public function getThemeManagerConfigVar(array $configVars, string $key): ?string
-    {
-        if (!array_key_exists($key, $configVars) || !is_string($value = $configVars[$key]) || !strlen($value))
-        {
-            return null;
-        }
-
-        return $value;
+        $this->generateImageTextWidths();
+        $this->generateArticleHeight();
+        $this->generateAspectRatios();
+        $this->generateBackgroundSizes();
+        $this->generateBackendCss();
     }
 
     /**
@@ -108,12 +65,117 @@ class ConfigGenerator
     }
 
     /**
+     * Gets a specific value from the ThemeManager configuration
+     */
+    public function getThemeManagerConfigVar(array $configVars, string $key): ?string
+    {
+        if (!array_key_exists($key, $configVars) || !is_string($value = $configVars[$key]) || !strlen($value))
+        {
+            return null;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Gets all image text widths for the image-text components adds it to the style-manager-tm-config.xml
+     */
+    private function generateImageTextWidths(): void
+    {
+        $options = self::getListOptions($this->configVars, 'image-text-ratio-options', 'it-width-', '%', [25,33,38,40,50,80,100]);
+
+        $this->xml->addGroup('cLayout')->addChild('imageTextWidth', $options);
+    }
+
+    /**
+     * Gets all vertical article heights from the ThemeManager configuration and adds it to the style-manager-tm-config.xml
+     */
+    private function generateArticleHeight(): void
+    {
+        $options    = self::getListOptions($this->configVars, 'article-options-vheight', 'a-vh-', 'vh', [50,75,100]);
+        $additional = self::getListOptions($this->configVars, 'article-options-height', 'a-h-');
+
+        $this->xml->addGroup('cArticleHeight')->addChild('height', array_merge($options, $additional));
+    }
+
+    /**
+     * Gets all aspect ratios from the ThemeManager configuration and adds it to the style-manager-tm-config.xml
+     */
+    private function generateAspectRatios(): void
+    {
+        $options = [];
+
+        // Fallback to default values
+        if (null === ($aspectRatios = self::getThemeManagerConfigVar($this->configVars, 'aspect-ratios')))
+        {
+            $aspectRatios = ['(4:5)','(1:1)','(4:3)','(3:2)','(16:10)','(16:9)'];
+        }
+        else
+        {
+            $aspectRatios = explode(',', $aspectRatios);
+        }
+
+        foreach ($aspectRatios as $value)
+        {
+            if (!!$value = substr($value, 1, -1))
+            {
+                if (2 === count($ratios = explode(':', $value)))
+                {
+                    $options[] = ['key' =>'ar-'.$ratios[0].'-'.$ratios[1],'value' => $value];
+                }
+            }
+        }
+
+        $this->xml->addGroup('eImage')->addChild('aspectRatio', $options);
+    }
+
+    /**
+     * Gets all background sizes from the ThemeManager configuration and adds it to the style-manager-tm-config.xml
+     */
+    private function generateBackgroundSizes(): void
+    {
+        if (null === ($additional = self::getThemeManagerConfigVar($this->configVars, 'background-sizes')))
+        {
+            return;
+        }
+
+        // Prefill defaults for order
+        $options = [['key' => 'bg--auto', 'value' => 'Auto'], ['key' => 'bg--contain', 'value' => 'Contain']];
+        $itemOptions = [['key' => 'i-bg--auto', 'value' => 'Auto'], ['key' => 'i-bg--contain', 'value' => 'Contain']];
+
+        foreach (explode(',', $additional) as $option)
+        {
+            if (!!$value = ltrim($option))
+            {
+                $sizes = explode(' ', $value);
+
+                $classSuffix = match(count($sizes))
+                {
+                    1 => $sizes[0],
+                    default => $sizes[0].'-'.$sizes[1],
+                };
+
+                $classSuffix = str_replace('%', 'pct', $classSuffix);
+
+                $options[] = ['key' => 'bgs--'. $classSuffix, 'value' => $value];
+                $itemOptions[] = ['key' => 'i-bgs--'. $classSuffix, 'value' => $value];
+            }
+        }
+
+        $this->xml->addGroup('gBackground')->addChild('size', $options);
+        $this->xml->addGroup('cBackground')->addChild('size', $options);
+        $this->xml->addGroup('eBackground')->addChild('size', $itemOptions);
+    }
+
+    /**
      * Generates the theme manager backend css
      *
      * @throws \Exception
      */
-    public function generateBackendCss($configVars): void
+    private function generateBackendCss(): void
     {
+        $configVars = $this->configVars;
+
         $css        = '';
         $bgColors   = ['primary','secondary','light','dark'];
         $textColors = ['text-color-regular' => 'color-text-base', 'text-color-invert' => 'color-text-inv'];
