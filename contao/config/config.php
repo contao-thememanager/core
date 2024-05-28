1e9b29c94411c9eabb0c5091870c8db75c8552d9
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-05-28 15:55:18 +0200

    Twig integration (see #34)
    * Twig integration
    
    - block searchable and unsearchable
    
    * Rewrite deserialize to function
    
    * Twig integration
    
    - downloads
    - hyperlink
    - table
    - teaser
    - toplink
    
    * Twig integration
    
    - headline
    
    * Use deserialize filter
    
    * Load headlineStyle
    
    * Twig integration
    
    - list
    
    * Twig integration
    
    - video/audio
    - youtube/vimeo
    - splashscreen
    
    * Twig integration
    
    - splashscreen
    
    * Twig integration
    
    - prepare lists
    - set image_container as default class
    
    * Twig integration
    
    - gallery
    
    * Twig integration
    
    - do not use linkClasses globally
    
    * Twig integration
    
    - download
    - downloads
    
    * CS
    
    * Update version
    
    * Update download components
    
    - do not change list-item behavior
    - do not stretch items when reversing order
    
    * Consider media positions for text-element
    
    * Add legacy classes for text and list
    
    * Change article_teaser headline to `h2`

--- contao/config/config.php
+++ contao/config/config.php
@@ -7,7 +7,6 @@
  */
 
 // Contao ThemeManager
-use Contao\CoreBundle\ContaoCoreBundle;
 use ContaoThemeManager\Core\Generator\BackgroundImageGenerator;
 use ContaoThemeManager\Core\Generator\ConfigGenerator;
 use ContaoThemeManager\Core\Generator\IconGenerator;
@@ -44,24 +43,3 @@ $GLOBALS['TL_WRAPPERS']['start'][] = 'wrapperStart';
 $GLOBALS['TL_WRAPPERS']['stop'][]  = 'wrapperStop';
 $GLOBALS['TL_WRAPPERS']['start'][] = 'wrapperStartContent';
 $GLOBALS['TL_WRAPPERS']['stop'][]  = 'wrapperStopContent';
-
-// Restore legacy elements -> Contao 5 compatibility since Twig is not supported by StyleManager yet
-// Will be removed with ThemeManager 2.1
-if (str_starts_with(ContaoCoreBundle::getVersion(), '5.'))
-{
-    $GLOBALS['TL_CTE']['texts']['code']      = \Contao\ContentCode::class;
-    $GLOBALS['TL_CTE']['texts']['headline']  = \Contao\ContentHeadline::class;
-    $GLOBALS['TL_CTE']['texts']['list']      = \Contao\ContentList::class;
-    $GLOBALS['TL_CTE']['texts']['text']      = \Contao\ContentText::class;
-    $GLOBALS['TL_CTE']['texts']['table']     = \Contao\ContentTable::class;
-    $GLOBALS['TL_CTE']['links']['hyperlink'] = \Contao\ContentHyperlink::class;
-    $GLOBALS['TL_CTE']['links']['toplink']   = \Contao\ContentToplink::class;
-    $GLOBALS['TL_CTE']['media']['image']     = \Contao\ContentImage::class;
-    $GLOBALS['TL_CTE']['media']['gallery']   = \Contao\ContentGallery::class;
-    $GLOBALS['TL_CTE']['media']['player']    = \Contao\ContentPlayer::class;
-    $GLOBALS['TL_CTE']['media']['youtube']   = \Contao\ContentYouTube::class;
-    $GLOBALS['TL_CTE']['media']['vimeo']     = \Contao\ContentVimeo::class;
-    $GLOBALS['TL_CTE']['files']['downloads'] = \Contao\ContentDownloads::class;
-    $GLOBALS['TL_CTE']['files']['download']  = \Contao\ContentDownload::class;
-    $GLOBALS['TL_CTE']['includes']['teaser'] = \Contao\ContentTeaser::class;
-}
