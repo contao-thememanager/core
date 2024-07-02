fb6ba6d16418b6b35f2873a0b9fdbbec43aa23ed
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-07-02 08:00:43 +0200

    Unlock theme-compiler `^1.8`

--- contao/classes/IconGenerator.php
+++ contao/classes/IconGenerator.php
@@ -12,7 +12,7 @@ use Contao\Config;
 use Contao\File;
 use Contao\StringUtil;
 use Contao\System;
-use Oveleon\ContaoThemeCompilerBundle\FileCompiler;
+use Oveleon\ContaoThemeCompilerBundle\Compiler\FileCompiler;
 use SimpleXMLElement;
 use Symfony\Component\Filesystem\Path;
 
--- contao/dca/tl_thememanager.php
+++ contao/dca/tl_thememanager.php
@@ -8,7 +8,7 @@
 use Contao\Backend;
 use Contao\BackendUser;
 use Contao\CoreBundle\Exception\AccessDeniedException;
-use Oveleon\ContaoThemeCompilerBundle\CompilerUtils;
+use Oveleon\ContaoThemeCompilerBundle\Utils\CompilerUtils;
 
 $GLOBALS['TL_DCA']['tl_thememanager'] = [
     // Config
